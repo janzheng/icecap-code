45ba52e7c37f93dcf1656c96e50e70892e38c308
Jan Zheng <janeazy@gmail.com>
2023-12-07 12:59:05 +1100

    test

--- icecream-dash/.svelte-kit/generated/client/nodes/5.js
+++
@@ -1,3 +0,0 @@
-import * as universal from "../../../../src/routes/examples/cards/+page.ts";
-export { universal };
-export { default as component } from "../../../../src/routes/examples/cards/+page.svelte";
